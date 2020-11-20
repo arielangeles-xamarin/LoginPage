b42fcb8bcac187ad57ed0e4235d6215e08d4c4ff
arielangeles <arielangeles123@hotmail.com>
2020-11-19 23:36:45 -0400

    make login page

---
+++ LoginPageXam/LoginPageXam/IndexPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginPageXam
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IndexPage : MasterDetailPage
+    {
+        public IndexPage(string username)
+        {
+            InitializeComponent();
+
+            textLabel.Text = $"Welcome, {username}";
+        }
+
+        async private void ButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
---
+++ LoginPageXam/LoginPageXam/LoginPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginPageXam
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : TabbedPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void SignInButtonClicked(object sender, EventArgs e)
+        {
+            if (password.Text == confirmPassword.Text || !string.IsNullOrEmpty(username.Text))
+            {
+                await Navigation.PushModalAsync(new IndexPage(username.Text));
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(username.Text))
+                {
+                    await DisplayAlert("Invalid username", "Insert a valid username, please", "Ok");
+                }
+                else
+                {
+                    await DisplayAlert("Invalid password", "Passwords are not the same", "Ok");
+                }
+
+            }
+
+        }
+    }
+}
